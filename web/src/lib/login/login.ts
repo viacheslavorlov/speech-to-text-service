c19300cf2682e32dd99dcfc3a5163385c6ccd46f
Orlov Viacheslav <viacheslavorlov@gmail.com>
2024-01-07 17:54:19 +0300

    login fix

--- web/src/lib/login/login.ts
+++ web/src/lib/login/login.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const loginUser = async (userData: { identifier: string; password: string }) => {
 	try {
-		const data = await axios.post('http://http://89.104.70.143:1337/api/auth/local', userData);
+		const data = await axios.post('http://89.104.70.143:1337/api/auth/local', userData);
 		const response = data;
 		if (response.data.jwt) {
 			return response;
