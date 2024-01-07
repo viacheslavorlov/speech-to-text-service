2abf0954cf2c9047a3c50f2012ed6a991539defe
Orlov Viacheslav <viacheslavorlov@gmail.com>
2024-01-07 16:38:30 +0300

    strapi cors fix 2

--- cms/config/middlewares.js
+++ cms/config/middlewares.js
@@ -1,15 +1,7 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: [ 'http://89.104.70.143:3000'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeaderOnError: true,
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
