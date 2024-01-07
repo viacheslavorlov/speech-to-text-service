4d2d292c7988194cf6dfa1e1036c73308e60f8bd
Orlov Viacheslav <viacheslavorlov@gmail.com>
2024-01-07 16:24:48 +0300

    deploy to reg.ru

--- cms/config/middlewares.js
+++ cms/config/middlewares.js
@@ -1,7 +1,15 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://89.104.70.143:1337', 'http://89.104.70.143:3000'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
